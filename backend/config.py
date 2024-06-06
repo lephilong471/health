a0f80ef23c01056791a95bdb85f3b04d907f1980
unknown <lephilong471@gmail.com>
2024-06-06 22:23:17 +0700

    Change to postgres

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 # pytesseract.pytesseract.tesseract_cmd = r'usr/bin/tesseract'
 DATABASE_HOST='localhost'
-DATABASE_PORT='3306'
+DATABASE_PORT='5432'
 DATABASE_NAME='health'
-DATABASE_USER='root'
+DATABASE_USER='postgres'
 DATABASE_PASSWORD='123456'
